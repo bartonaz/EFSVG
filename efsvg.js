54ac6c630d226180ebbfbbeb43f490ee56142533
Nazar Bartosik <nazar.bartosik@gmail.com>
2013-07-01 00:43:38 +0200

    Added main properties outline.

--- efsvg.js
+++ efsvg.js
@@ -21,15 +21,19 @@ var E = function(doc){
 			if(!isTypeEcorrect(input)) return;
 			var props = {};
 			props.type = input;
-			setPropsE(el,"")
+			setPropsE(el)
+			setSVGAccessors(el);
 			return el;
 		} else if(typeof input === "object") {
 			var props = input;
 			if(!isTypeEcorrect(props.type)) return;
-
+			var el = e.el(props.type);
+			delete props.type;
+			setPropsE(el,props);
+			setSVGAccessors(el);
 		}
 		if(e.log) console.log("Wrong input in el(): '"+input+"'");
-	}
+	};
 
 	//////////////////
 	// PRIVATE METHODS
@@ -47,31 +51,44 @@ var E = function(doc){
 		if(typesE.indexOf(type) !== -1) return true;
 		if(e.log) console.warn("Wrong type of the E element: '"+type+"'");
 		return false;
-	}
+	};
 
 	// All possible properties of the E element
 	var propsE = {
-		"Etype":"rect",
-		"Edx":10,
-		"Edy":10,
-		"Epoints":[[0,0],[10,10]],
-		"Eclosed":true,
-		"Evisible":true
+		"Etype":"rect",							// Type of the element
+		"Edx":10,								// Size X
+		"Edy":10,								// Size Y
+		"Epoints":[[0,0],[10,10]],				// Array of the points
+		"Efrac":100,							// Fraction starting from the 1-st point that should be drawn
+		"Evisible":true							// Whether the object should be visible
 	};
+
+	// All possible properties of the SVG element
+	var propsSVG = {};
 
 	// Creates true SVG element in the DOM
 	var createDOMel = function(name){
 		var el = doc.createElementNS(this.ns, name);
 		return el;
-	}
+	};
 
 	// Sets all properties of the E element
 	var setPropsE = function(el,props){
 		for(key in propsE){
-			if(!propsE.hasOwnProperty(key)) continue;
-			el[key] = propsE[key];
+			if(!propsE.hasOwnProperty(key)) {
+				if(log) console.warn("Wrong property name '"+key+"'' for object of type '"+el.Etype+"'");
+				continue;
+			}
+			if(el.hasOwnProperty(key)) {
+				el[key] = props[key];
+			} else el[key] = propsE[key];
 		}
-	}
+	};
+
+	// Sets all accessors to the attributes of the SVG element
+	var setSVGAccessors = function(el){
+
+	};
 
 
 	return e;
--- test/test1.js
+++ test/test1.js
@@ -1,3 +1,18 @@
 E.log = true;
 var svg = document.getElementById("svg");
 var c1 = E.el("circle");
+
+var c0 = document.getElementById("c0");
+
+
+Object.defineProperty(c0, "Er1",{
+	get: function(){return this.getAttribute("r");},
+	set: function(v){this.setAttribute("r",v);}
+});
+
+c0.ErNode = c0.getAttributeNode("r");
+
+Object.defineProperty(c0, "Er2",{
+	get: function(){return this.ErNode.nodeValue;},
+	set: function(v){this.ErNode.nodeValue = v;}
+});
