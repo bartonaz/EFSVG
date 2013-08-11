77c781ed0f28d420c10a56d43f8d5da04bfbc0ca
Nazar Bartosik <nazar.bartosik@gmail.com>
2013-08-11 22:03:22 +0200

    Added missing folders with classes

---
+++ src/utils/dom.js
@@ -0,0 +1,20 @@
+/**
+ * @module JSUTILS
+ */
+
+var JSUTILS;
+if (!JSUTILS) JSUTILS = {};
+
+/**
+ * Javascript utilities for working with DOM
+ * @class Dom
+ */
+JSUTILS.Dom = (function (doc) {
+    
+    "use strict";
+
+    // Public instance of the class
+    var U = {};
+
+    return U;
+}(document || this))
